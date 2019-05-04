50e078c76cd08a80ddc26c58851c8bc4e07a8872
Thomas Maurhart <thomas.maurhart@gmail.com>
2019-05-04 22:35:34 +0200

    changed icons

--- docs/app.js
+++ docs/app.js
@@ -1,11 +1,12 @@
 // Detects if device is on iOS 
 const isIos = () => {
     const userAgent = window.navigator.userAgent.toLowerCase();
-    console.log(userAgent);
     return /iphone|ipad|ipod/.test(userAgent);
 }
 // Detects if device is in standalone mode
 const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+//console.log(isInStandaloneMode);
 
 // Checks if should display install popup notification:
 if (isIos() && !isInStandaloneMode()) {
