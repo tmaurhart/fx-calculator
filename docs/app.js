648e9f6bfe9f855b1745ac988f85b31cdd5adf45
Thomas Maurhart <thomas.maurhart@gmail.com>
2019-05-04 11:17:36 +0200

    init

---
+++ docs/app.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('select').forEach(element => {
+    element.innerHTML = `
+        <option value="EUR">EUR</option>
+        <option value="USD">USD</option>
+        <option value="CHF">CHF</option>
+        <option value="SEK">SEK</option>
+        `;
+});
